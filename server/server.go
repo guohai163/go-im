9a86f06e888a19c0f32a85f81553af016bef660c
郭海 <guohai@GH-CMINI.local>
2024-04-30 18:12:16 +0800

    first commit

---
+++ server/server.go
@@ -0,0 +1,245 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"text/template"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", ":8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default option
+
+type Data struct {
+	Ip       string   `json:"ip"`
+	User     string   `json:"user"`
+	From     string   `json:"from"`
+	Type     string   `json:"type"`
+	Content  string   `json:"content"`
+	UserList []string `json:"user_list"`
+}
+
+type connection struct {
+	ws   *websocket.Conn
+	sc   chan []byte
+	data *Data
+}
+
+var h = hub{
+	c: make(map[*connection]bool),
+	u: make(chan *connection),
+	b: make(chan []byte),
+	r: make(chan *connection),
+}
+
+type hub struct {
+	c map[*connection]bool
+	b chan []byte
+	r chan *connection
+	u chan *connection
+}
+
+func echo(ctx *gin.Context) {
+	w, r := ctx.Writer, ctx.Request
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("upgrade:", err)
+		return
+	}
+
+	c := &connection{sc: make(chan []byte, 256), ws: ws, data: &Data{}}
+	h.r <- c
+	go c.writer()
+	c.reader()
+	defer func() {
+		c.data.Type = "logout"
+		user_list = del(user_list, c.data.User)
+		c.data.UserList = user_list
+		c.data.Content = c.data.User
+		data_b, _ := json.Marshal(c.data)
+		h.b <- data_b
+		h.r <- c
+	}()
+}
+func (h *hub) run() {
+	for {
+		select {
+		case c := <-h.r:
+			h.c[c] = true
+			c.data.Ip = c.ws.RemoteAddr().String()
+			c.data.Type = "handshake"
+			c.data.UserList = user_list
+			data_b, _ := json.Marshal(c.data)
+			c.sc <- data_b
+		case c := <-h.u:
+			if _, ok := h.c[c]; ok {
+				delete(h.c, c)
+				close(c.sc)
+			}
+		case data := <-h.b:
+			for c := range h.c {
+				select {
+				case c.sc <- data:
+				default:
+					delete(h.c, c)
+					close(c.sc)
+				}
+			}
+		}
+	}
+}
+
+var user_list = []string{}
+
+func (c *connection) reader() {
+	for {
+		_, message, err := c.ws.ReadMessage()
+		if err != nil {
+			h.r <- c
+			break
+		}
+		json.Unmarshal(message, &c.data)
+		switch c.data.Type {
+		case "login":
+			c.data.User = c.data.Content
+			c.data.From = c.data.User
+			user_list = append(user_list, c.data.User)
+			c.data.UserList = user_list
+			data_b, _ := json.Marshal(c.data)
+			h.b <- data_b
+		case "user":
+			c.data.Type = "user"
+			data_b, _ := json.Marshal(c.data)
+			h.b <- data_b
+		case "logout":
+			c.data.Type = "logout"
+			user_list = del(user_list, c.data.User)
+			data_b, _ := json.Marshal(c.data)
+			h.b <- data_b
+			h.r <- c
+		default:
+			fmt.Print("========default================")
+		}
+	}
+}
+
+func del(slice []string, user string) []string {
+	count := len(slice)
+	if count == 0 {
+		return slice
+	}
+	if count == 1 && slice[0] == user {
+		return []string{}
+	}
+	var n_slice = []string{}
+	for i := range slice {
+		if slice[i] == user && i == count {
+			return slice[:count]
+		} else if slice[i] == user {
+			n_slice = append(slice[:i], slice[i+1:]...)
+			break
+		}
+	}
+	fmt.Println(n_slice)
+	return n_slice
+}
+
+func (c *connection) writer() {
+	for message := range c.sc {
+		c.ws.WriteMessage(websocket.TextMessage, message)
+	}
+	c.ws.Close()
+}
+
+func home(c *gin.Context) {
+	homeTemplate.Execute(c.Writer, "ws://"+c.Request.Host+"/echo")
+}
+
+func main() {
+	flag.Parse()
+	go h.run()
+	log.SetFlags(0)
+	r := gin.Default()
+	r.GET("/echo", echo)
+	r.GET("/", home)
+	log.Fatal(r.Run(*addr))
+}
+
+var homeTemplate = template.Must(template.New("").Parse(`
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script>  
+window.addEventListener("load", function(evt) {
+    var output = document.getElementById("output");
+    var input = document.getElementById("input");
+    var ws;
+    var print = function(message) {
+        var d = document.createElement("div");
+        d.textContent = message;
+        output.appendChild(d);
+        output.scroll(0, output.scrollHeight);
+    };
+    document.getElementById("open").onclick = function(evt) {
+        if (ws) {
+            return false;
+        }
+        ws = new WebSocket("{{.}}");
+        ws.onopen = function(evt) {
+            print("OPEN");
+        }
+        ws.onclose = function(evt) {
+            print("CLOSE");
+            ws = null;
+        }
+        ws.onmessage = function(evt) {
+            print("RESPONSE: " + evt.data);
+        }
+        ws.onerror = function(evt) {
+            print("ERROR: " + evt.data);
+        }
+        return false;
+    };
+    document.getElementById("send").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        print("SEND: " + input.value);
+        ws.send(input.value);
+        return false;
+    };
+    document.getElementById("close").onclick = function(evt) {
+        if (!ws) {
+            return false;
+        }
+        ws.close();
+        return false;
+    };
+});
+</script>
+</head>
+<body>
+<table>
+<tr><td valign="top" width="50%">
+<p>Click "Open" to create a connection to the server, 
+"Send" to send a message to the server and "Close" to close the connection. 
+You can change the message and send multiple times.
+<p>
+<form>
+<button id="open">Open</button>
+<button id="close">Close</button>
+<p><input id="input" type="text" value="Hello world!">
+<button id="send">Send</button>
+</form>
+</td><td valign="top" width="50%">
+<div id="output" style="max-height: 70vh;overflow-y: scroll;"></div>
+</td></tr></table>
+</body>
+</html>
+`))
